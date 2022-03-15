5e7fad62717522c4be59a2e365b16983af25124a
Shaodn <42764722+Shaodn@users.noreply.github.com>
2022-03-15 19:53:53 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,3 +1,20 @@
 (function(){
-  console.log('dandan')
+  console.log('dandan2')
+  $('h1').click(function(){
+    $.ajax({
+		type: "get",
+		url: "https://10.96.64.48/fei.php",
+		data: {name:'fileNamaSelect'},
+		
+		//请求成功
+		success : function(data1) {
+			console.log('success'+data1)
+            
+		},
+		//请求失败，包含具体的错误信息
+		error : function(e){
+			console.log(e);
+		}
+	});
+  })
 })()

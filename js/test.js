2457ab43410d2e3ec8ffb1078ead6f78ef213655
Shaodn <42764722+Shaodn@users.noreply.github.com>
2022-03-15 19:58:51 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,19 +2,24 @@
   console.log('dandan2')
   $('h1').click(function(){
     $.ajax({
-		type: "get",
-		url: "https://10.96.64.48/fei.php",
-		data: {name:'fileNamaSelect'},
-		
-		//请求成功
-		success : function(data1) {
-			console.log('success'+data1)
-            
-		},
-		//请求失败，包含具体的错误信息
-		error : function(e){
-			console.log(e);
-		}
-	});
+        //请求方式
+        type : "GET",
+        //请求的媒体类型
+        //请求地址
+        url : "https://10.96.64.48/fei.php",
+        
+        header:{
+        "Access-Control-Allow-Origin":"*"
+        },
+        crossDomain: true,
+        //请求失败，包含具体的错误信息
+        success:function(){
+            console.log('success')
+        },
+        error : function(e){
+            console.log(e);
+            console.log(e.responseText);
+        }
+    });
   })
 })()
